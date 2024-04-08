7d8701cac46a8f14fd8c69ef46700023c0ea52d9
midhundais <midhun@cloudstick.io>
2024-04-08 22:32:34 +0530

    second

---
+++ App.js
@@ -0,0 +1,9 @@
+
+
+
+
+const heading = React.createElement(
+    "h1", { id: "heading", xyz: 'fdsdgd' }, "new hello world from react ");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading);
